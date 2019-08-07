4d79eeb702a6eec78bda35a03f744cd49c701f12
Tasmia Aqila <ornicse@gmai.com>
2019-08-07 19:24:31 -0400

    Testing image read and display

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<string>
+#include<opencv2/opencv.hpp>
 using namespace std;
-int main(int argc, char** argv) {
+using namespace cv;
+int main(int argc, char* argv) {
 	printf("Hello world\n");
-	system("pause");
+	Mat img;
+	img = imread("lights.jpg");
+	if (img.empty()) {
+		printf("Error in reading images\n");
+		return 0;
+	}
+	string windowName = "Display an image";
+	namedWindow(windowName,WINDOW_AUTOSIZE);
+	imshow(windowName,img);
+	waitKey();
+	//system("pause");
 	return 0;
 }
